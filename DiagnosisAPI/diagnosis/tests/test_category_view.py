ebe3c62392e1b8712bd78f916e6d44433ee69b6f
Ibrahim Shittu <ibshittu01@gmail.com>
2022-01-15 22:22:08 +0100

    unit tests

--- DiagnosisAPI/diagnosis/tests/test_category_view.py
+++ DiagnosisAPI/diagnosis/tests/test_category_view.py
@@ -1,95 +1,101 @@
 from rest_framework.test import APITestCase
+from rest_framework import status
 from django.urls import reverse
 
 
 class CategoryViewTest(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('category-list')
+        self.valid_data = {'code': 'CAT1', 'title': 'Category 1'}
+        self.update_data = {'code': 'CAT2', 'title': 'Category 2'}
+        self.invalid_data = {'code': '', 'title': ''}
+
     def test_create_category(self):
         self.url = reverse('category-list')
         response = self.client.post(
-            self.url, {'code': 'CAT1', 'title': 'Category 1'}, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'CAT1')
         self.assertEqual(response.data['title'], 'Category 1')
 
     def test_create_category_with_empty_data(self):
         self.url = reverse('category-list')
         response = self.client.post(
-            self.url, {'code': '', 'title': ''}, format='json')
-        self.assertEqual(response.status_code, 400)
+            self.url, self.invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['code'][0],
                          'This field may not be blank.')
 
     def test_delete_category(self):
         self.url = reverse('category-list')
         response = self.client.post(
-            self.url, {'code': 'CAT1', 'title': 'Category 1'}, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'CAT1')
         self.assertEqual(response.data['title'], 'Category 1')
         self.url = reverse('category-detail', args=[response.data['id']])
         response = self.client.delete(self.url)
-        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_delete_category_with_invalid_id(self):
         self.url = reverse('category-detail', args=[9999])
         response = self.client.delete(self.url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_update_category(self):
         self.url = reverse('category-list')
         response = self.client.post(
-            self.url, {'code': 'CAT1', 'title': 'Category 1'}, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'CAT1')
         self.assertEqual(response.data['title'], 'Category 1')
         self.url = reverse('category-detail', args=[response.data['id']])
         response = self.client.put(
-            self.url, {'code': 'CAT2', 'title': 'Category 2'}, format='json')
-        self.assertEqual(response.status_code, 200)
+            self.url, self.update_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['code'], 'CAT2')
         self.assertEqual(response.data['title'], 'Category 2')
 
     def test_update_category_with_empty_data(self):
         self.url = reverse('category-list')
         response = self.client.post(
-            self.url, {'code': 'CAT1', 'title': 'Category 1'}, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'CAT1')
         self.assertEqual(response.data['title'], 'Category 1')
         self.url = reverse('category-detail', args=[response.data['id']])
         response = self.client.put(
-            self.url, {'code': '', 'title': ''}, format='json')
-        self.assertEqual(response.status_code, 400)
+            self.url, self.invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['code'][0],
                          'This field may not be blank.')
 
     def test_get_category(self):
         self.url = reverse('category-list')
         response = self.client.post(
-            self.url, {'code': 'CAT1', 'title': 'Category 1'}, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'CAT1')
         self.assertEqual(response.data['title'], 'Category 1')
         self.url = reverse('category-detail', args=[response.data['id']])
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['code'], 'CAT1')
         self.assertEqual(response.data['title'], 'Category 1')
 
     def test_get_category_with_invalid_id(self):
         self.url = reverse('category-detail', args=[9999])
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_get_category_list(self):
         self.url = reverse('category-list')
         response = self.client.post(
-            self.url, {'code': 'CAT1', 'title': 'Category 1'}, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'CAT1')
         self.assertEqual(response.data['title'], 'Category 1')
         self.url = reverse('category-list')
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data[0]['code'], 'CAT1')
-        self.assertEqual(response.data[0]['title'], 'Category 1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- DiagnosisAPI/diagnosis/tests/test_diagnosis_view.py
+++ DiagnosisAPI/diagnosis/tests/test_diagnosis_view.py
@@ -1,35 +1,48 @@
 from rest_framework.test import APITestCase
+from rest_framework import status
 from django.urls import reverse
-from diagnosis.models import Diagnosis, Category
+from diagnosis.models import Category
 
 
 class DiagnosisViewTest(APITestCase):
-    def test_create_diagnosis(self):
+
+    def setUp(self):
         self.url = reverse('diagnosis-list')
-        category = Category.objects.create(code='CAT1', title='Category 1')
-        data = {
-            'category': category.id,
+        self.category = Category.objects.create(
+            code='CAT1', title='Category 1')
+
+        self.valid_data = {
+            'category': self.category.id,
             'code': 'DIA1',
             'description': 'Diagnosis 1',
             'code_type': 'ICD_10'
         }
-        response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data['code'], 'DIA1')
-        self.assertEqual(response.data['description'], 'Diagnosis 1')
 
-    def test_create_diagnosis_with_empty_data(self):
-        self.url = reverse('diagnosis-list')
-        data = {
+        self.update_data = {
+            'category': self.category.id,
+            'code': 'DIA2',
+            'description': 'Diagnosis 2',
+            'code_type': 'ICD_10'
+        }
+
+        self.invalid_data = {
             'category': '',
             'code': '',
             'description': '',
             'code_type': 'ICD_10'
         }
+
+    def test_create_diagnosis(self):
         response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 400)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['code'], 'DIA1')
+        self.assertEqual(response.data['description'], 'Diagnosis 1')
+
+    def test_create_diagnosis_with_empty_data(self):
+        response = self.client.post(
+            self.url, self.invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['category'][0],
                          'This field may not be null.')
         self.assertEqual(response.data['code'][0],
@@ -38,74 +51,38 @@ class DiagnosisViewTest(APITestCase):
                          'This field may not be blank.')
 
     def test_delete_diagnosis(self):
-        self.url = reverse('diagnosis-list')
-        category = Category.objects.create(code='CAT1', title='Category 1')
-        data = {
-            'category': category.id,
-            'code': 'DIA1',
-            'description': 'Diagnosis 1',
-            'code_type': 'ICD_10'
-        }
         response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'DIA1')
         self.assertEqual(response.data['description'], 'Diagnosis 1')
         self.url = reverse('diagnosis-detail', args=[response.data['id']])
         response = self.client.delete(self.url)
-        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_update_diagnosis(self):
-        self.url = reverse('diagnosis-list')
-        category = Category.objects.create(code='CAT1', title='Category 1')
-        data = {
-            'category': category.id,
-            'code': 'DIA1',
-            'description': 'Diagnosis 1',
-            'code_type': 'ICD_10'
-        }
         response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'DIA1')
         self.assertEqual(response.data['description'], 'Diagnosis 1')
         self.url = reverse('diagnosis-detail', args=[response.data['id']])
-        data = {
-            'category': category.id,
-            'code': 'DIA2',
-            'description': 'Diagnosis 2',
-            'code_type': 'ICD_10'
-        }
         response = self.client.put(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 200)
+            self.url, self.update_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['code'], 'DIA2')
         self.assertEqual(response.data['description'], 'Diagnosis 2')
 
     def test_update_diagnosis_with_empty_data(self):
-        self.url = reverse('diagnosis-list')
-        category = Category.objects.create(code='CAT1', title='Category 1')
-        data = {
-            'category': category.id,
-            'code': 'DIA1',
-            'description': 'Diagnosis 1',
-            'code_type': 'ICD_10'
-        }
         response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'DIA1')
         self.assertEqual(response.data['description'], 'Diagnosis 1')
         self.url = reverse('diagnosis-detail', args=[response.data['id']])
-        data = {
-            'category': '',
-            'code': '',
-            'description': '',
-            'code_type': 'ICD_10'
-        }
         response = self.client.put(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 400)
+            self.url, self.invalid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['category'][0],
                          'This field may not be null.')
         self.assertEqual(response.data['code'][0],
@@ -114,57 +91,30 @@ class DiagnosisViewTest(APITestCase):
                          'This field may not be blank.')
 
     def test_get_diagnosis_list(self):
-        self.url = reverse('diagnosis-list')
-        category = Category.objects.create(code='CAT1', title='Category 1')
-        data = {
-            'category': category.id,
-            'code': 'DIA1',
-            'description': 'Diagnosis 1',
-            'code_type': 'ICD_10'
-        }
         response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'DIA1')
         self.assertEqual(response.data['description'], 'Diagnosis 1')
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
-        self.assertEqual(response.data[0]['code'], 'DIA1')
-        self.assertEqual(response.data[0]['description'], 'Diagnosis 1')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_get_diagnosis_detail(self):
-        self.url = reverse('diagnosis-list')
-        category = Category.objects.create(code='CAT1', title='Category 1')
-        data = {
-            'category': category.id,
-            'code': 'DIA1',
-            'description': 'Diagnosis 1',
-            'code_type': 'ICD_10'
-        }
         response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'DIA1')
         self.assertEqual(response.data['description'], 'Diagnosis 1')
         self.url = reverse('diagnosis-detail', args=[response.data['id']])
         response = self.client.get(self.url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_delete_diagnosis_detail(self):
-        self.url = reverse('diagnosis-list')
-        category = Category.objects.create(code='CAT1', title='Category 1')
-        data = {
-            'category': category.id,
-            'code': 'DIA1',
-            'description': 'Diagnosis 1',
-            'code_type': 'ICD_10'
-        }
         response = self.client.post(
-            self.url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+            self.url, self.valid_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['code'], 'DIA1')
         self.assertEqual(response.data['description'], 'Diagnosis 1')
         self.url = reverse('diagnosis-detail', args=[response.data['id']])
         response = self.client.delete(self.url)
-        self.assertEqual(response.status_code, 204)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
