afa15b6280ac6853b6826946c5b7d08399432d76
Ibrahim Shittu <ibshittu01@gmail.com>
2022-01-18 09:01:02 +0100

    docker

--- DiagnosisAPI/DiagnosisAPI/settings.py
+++ DiagnosisAPI/DiagnosisAPI/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import environ
-# Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
